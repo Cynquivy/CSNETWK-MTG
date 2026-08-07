100f1d40718376fb8347669e0048530f0fd5e303
AmielSTongco <amiel_s_tongco@dlsu.edu.ph>
2026-08-08 00:03:02 +0800

    Added game state as strings

--- network/server.py
+++ network/server.py
@@ -1,7 +1,6 @@
 import argparse
 import socket
 import threading
-from enum import Enum
 
 from network import protocol
 from network.protocol import (Connection, ConnectionClosed, ProtocolError, log)
@@ -15,11 +14,7 @@ from model.instant import instant
 from model.sorcery import sorcery
 
 MAX_PLAYERS = 2
-
-class ServerState(Enum):
-    LOBBY = "LOBBY"
-    SETUP = "SETUP"
-    IN_GAME = "IN_GAME"
+LOBBY, SETUP, IN_GAME = "LOBBY", "SETUP", "IN_GAME"
 
 class GameServer:
     def __init__(self, host: str, port: int, verbose: bool):
@@ -30,7 +25,7 @@ class GameServer:
         self.lock = threading.Lock()
         # controls flow of game
         self.session = GameSession()
-        self.state = ServerState.LOBBY
+        self.state = LOBBY
 
         # handler table to call the corresponding method of each pdu type
         self._handlers = {
@@ -133,7 +128,7 @@ class GameServer:
 
     ### HANDLERS ###
     def _handle_player_ready(self, label: str, conn: Connection, pdu: dict) -> None:
-        if self.state != ServerState.LOBBY:
+        if self.state != LOBBY:
             conn.send_pdu({
                 "type": "ERROR",
                 "code": "WRONG_PHASE",
